3a957457cb685fce9a2d68993e60d48ef576d83d
文优 <thisfin@gmail.com>
2017-01-06 18:26:51 +0800

    Add

---
+++ SnapKitNSTest/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  SnapKitNSTest
+//
+//  Created by wenyou on 2017/1/6.
+//  Copyright © 2017年 wenyou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    static let windowSize = NSMakeSize(800, 500)
+    var window: NSWindow!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        window = NSWindow(contentRect: NSRect.zero,
+                          styleMask: [.closable, .resizable, .miniaturizable, .titled],
+                          backing: .buffered,
+                          defer: false)
+        window.contentViewController = ViewController()
+        window.center()
+        window.makeKeyAndOrderFront(self)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
